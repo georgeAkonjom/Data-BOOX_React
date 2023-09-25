acfab46f8d18d0245781f1e2c53c021b27f6427d
samuel-george <georgeakonjom003@gmail.com>
2023-09-25 04:29:11 +0100

    carousel section respnsiveness bug fix

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -1,47 +1,117 @@
 import TextGroup from "./TextGroup";
-import CarouselChild from "./CarouselChild";
 import "../styles/carousel.css";
 
 function Carousel() {
 	return (
-		<div className="dark container">
+		<div className="dark">
 			<TextGroup
 				topic="How our partners use DataBOOX"
 				tag="OUR IMPACT"
 				subtext=""
 			/>
-
-			<div className="scrollgrid">
-				<CarouselChild
-					imgPath="/rectangle-120-lady.png"
-					logoPath="/groupiitawhite.png"
-					subtext="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-				/>
-				<CarouselChild
-					imgPath="/Rectangle 119green.png"
-					logoPath="/groupcswhite.png"
-					subtext="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-				/>
-				<CarouselChild
-					imgPath="/Rectangle 119green.png"
-					logoPath="/groupcswhite.png"
-					subtext="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-				/>
-				<CarouselChild
-					imgPath="/Rectangle 119green.png"
-					logoPath="/groupcswhite.png"
-					subtext="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-				/>
-				<CarouselChild
-					imgPath="/Rectangle 119green.png"
-					logoPath="/groupcswhite.png"
-					subtext="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-				/>
-				<CarouselChild
-					imgPath="/Rectangle 119green.png"
-					logoPath="/groupcswhite.png"
-					subtext="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took."
-				/>
+			<div className="scrollgrid container">
+				<div className="scrollchild">
+					<img
+						className="logo-partner"
+						src="/groupcswhite.png"
+						alt="A partner or client logo"
+					/>
+					<img
+						className="carousel-image"
+						src="/Rectangle 119green.png"
+						alt="An image in a carousel"
+					/>
+					<p className="subtext">
+						Lorem Ipsum has been the industry's standard
+						dummy text ever since the 1500s, when an unknown
+						printer took.
+					</p>
+				</div>
+				<div className="scrollchild">
+					<img
+						className="logo-partner"
+						src="/groupcswhite.png"
+						alt="A partner or client logo"
+					/>
+					<img
+						className="carousel-image"
+						src="/Rectangle 119green.png"
+						alt="An image in a carousel"
+					/>
+					<p className="subtext">
+						Lorem Ipsum has been the industry's standard
+						dummy text ever since the 1500s, when an unknown
+						printer took.
+					</p>
+				</div>
+				<div className="scrollchild">
+					<img
+						className="logo-partner"
+						src="/groupcswhite.png"
+						alt="A partner or client logo"
+					/>
+					<img
+						className="carousel-image"
+						src="/Rectangle 119green.png"
+						alt="An image in a carousel"
+					/>
+					<p className="subtext">
+						Lorem Ipsum has been the industry's standard
+						dummy text ever since the 1500s, when an unknown
+						printer took.
+					</p>
+				</div>
+				<div className="scrollchild">
+					<img
+						className="logo-partner"
+						src="/groupcswhite.png"
+						alt="A partner or client logo"
+					/>
+					<img
+						className="carousel-image"
+						src="/Rectangle 119green.png"
+						alt="An image in a carousel"
+					/>
+					<p className="subtext">
+						Lorem Ipsum has been the industry's standard
+						dummy text ever since the 1500s, when an unknown
+						printer took.
+					</p>
+				</div>
+				<div className="scrollchild">
+					<img
+						className="logo-partner"
+						src="/groupcswhite.png"
+						alt="A partner or client logo"
+					/>
+					<img
+						className="carousel-image"
+						src="/Rectangle 119green.png"
+						alt="An image in a carousel"
+					/>
+					<p className="subtext">
+						Lorem Ipsum has been the industry's standard
+						dummy text ever since the 1500s, when an unknown
+						printer took.
+					</p>
+				</div>
+				<div className="scrollchild">
+					<img
+						className="logo-partner"
+						src="/groupcswhite.png"
+						alt="A partner or client logo"
+					/>
+					<img
+						className="carousel-image"
+						src="/Rectangle 119green.png"
+						alt="An image in a carousel"
+					/>
+					<p className="subtext">
+						Lorem Ipsum has been the industry's standard
+						dummy text ever since the 1500s, when an unknown
+						printer took.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
